c600cb1e36dfcd65424dba19b32eb82f988da7cc
Gareth Rees <gareth@garethrees.co.uk>
2016-09-13 22:44:32 +0100

    Use an instance of Calculator

--- lib/readingtime.rb
+++ lib/readingtime.rb
@@ -9,50 +9,38 @@ module Readingtime
   end
 
   def self.hms(secs)
-    h, m, s = 0, 0, 0
-    h = secs / 3600
-    secs -= h * 3600
-    m = secs / 60
-    secs -= m * 60
-    [h, m, secs]
+    calculator = Calculator.new(:reading_speed => self.reading_speed)
+    calculator.hms(secs)
   end
 
   def self.minutes_in_seconds(words)
-    (words / self.reading_speed).floor * 60
+    calculator = Calculator.new(:reading_speed => self.reading_speed)
+    calculator.minutes_in_seconds(words)
   end
 
   def self.seconds(words)
-    (words % self.reading_speed / (self.reading_speed / 60)).floor
+    calculator = Calculator.new(:reading_speed => self.reading_speed)
+    calculator.seconds(words)
   end
 
   # TODO: Account for HH:MM:00
   def self.format_seconds(seconds)
-    '%02d:%02d' % seconds.divmod(60)
+    calculator = Calculator.new(:reading_speed => self.reading_speed)
+    calculator.format_seconds(seconds)
   end
 
   def self.format_words(seconds)
-    if seconds >= 60
-      '%d minutes and %d seconds' % seconds.divmod(60)
-    else
-      "#{ seconds } seconds"
-    end
-    
+    calculator = Calculator.new(:reading_speed => self.reading_speed)
+    calculator.format_words(seconds)
   end
 
   def self.format_approx(seconds)
-    if seconds > 59
-      '%d minutes' % (seconds.to_f/60).round
-    else 
-      '%d seconds' % seconds
-    end
+    calculator = Calculator.new(:reading_speed => self.reading_speed)
+    calculator.format_approx(seconds)
   end
 
   def self.format_full(hms)
-    r, h, m, s = [], hms[0], hms[1], hms[2]
-    r << "#{h} #{h == 1 ? 'hr' : 'hrs'}" if h > 0
-    r << "#{m} #{m == 1 ? 'min' : 'mins'}" if m > 0
-    r << "#{s} #{s == 1 ? 'sec' : 'secs'}" if s > 0
-    r.join(" ")
+    calculator = Calculator.new(:reading_speed => self.reading_speed)
+    calculator.format_full(hms)
   end
-
 end
--- lib/readingtime/core_ext.rb
+++ lib/readingtime/core_ext.rb
@@ -1,29 +1,11 @@
 String.class_eval do
   def reading_time(options = {})
+    calculator = options[:calculator] || Readingtime::Calculator.new
     format_options = options[:format] || :basic
-
-    word_size = self.calculate_size
-    minutes = Readingtime.minutes_in_seconds(word_size)
-    seconds = Readingtime.seconds(word_size)
-
-    case format_options
-    when :basic
-      Readingtime.format_seconds((minutes + seconds))
-    when :long
-      Readingtime.format_words((minutes + seconds))
-    when :approx
-      Readingtime.format_approx((minutes + seconds))
-    when :full
-      hms = Readingtime.hms(minutes + seconds)
-      Readingtime.format_full(hms)
-    when :raw
-      Readingtime.hms(minutes + seconds)
-    end
-
+    calculator.reading_time(self, :format => format_options)
   end
 
   def calculate_size
     self.scan(/(\w|-)+/).size
   end
-
 end
